e591b78158f5e4c19f893e16c8e82ba59161279c
Peter McNeely <pete.mcneely@gmail.com>
2016-05-13 23:29:12 -0400

    Improve login tests.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-  describe "GET index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
+  context 'logged in user' do
+    login_user
+
+    describe "GET index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+    end
+
+    describe "GET about" do
+      it "renders the about page" do
+        get :about
+        expect(response).to render_template("about")
+      end
     end
   end
 
-  describe "GET about" do
-    it "renders the about template" do
-      get :about
-      expect(response).to render_template("about")
+  context 'non-logged in user' do
+    describe "GET index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+    end
+
+    describe "GET about" do
+      it "redirects user to log in" do
+        get :about
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
