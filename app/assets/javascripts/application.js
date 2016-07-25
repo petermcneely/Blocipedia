2d84fdb529f2e9cd26682011cac7c2406d6d6ee0
Peter McNeely <pete.mcneely@gmail.com>
2016-07-24 21:49:52 -0400

    Getting blocmetrics to work on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,19 @@
 //= require bootstrap
 //= require epic_editor/epic_editor
 //= require_tree .
+
+var blocmetrics = {};
+
+blocmetrics.report = function(eventName) {
+   // #1
+   var event = {event: { name: eventName }};
+ 
+   // #2
+   var request = new XMLHttpRequest();
+   // #3
+   request.open("POST", "https://bk-blocmetrics.herokuapp.com/api/events", true);
+   // #4
+   request.setRequestHeader('Content-Type', 'application/json');
+   // #5
+   request.send(JSON.stringify(event));
+}
